0e8b2e92e70b6f05ef3e7dcaa676fef83b96925c
JC52036 <joydeep327@gmail.com>
2018-10-27 03:49:45 +0530

    Initial commit

---
+++ src/com/test/java/FirstJava.java
@@ -0,0 +1,7 @@
+package com.test.java;
+
+public class FirstJava {
+    public static void main(String[] args) {
+      System.out.println("Hello Everyone");
+    }
+}
---
+++ src/excellSheet/TestJava.java
@@ -0,0 +1,5 @@
+package excellSheet;
+
+public class TestJava {
+
+}
